464b0684fffdc702262521969fb2721e76204b60
sriyayele <30803620+sriyayele@users.noreply.github.com>
2017-08-10 22:53:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function(req, res) {
+    res.send('article-one requested and served');
+});
+
+app.get('/article-two', function(req, res) {
+    res.send('article-two requested and served');
+});
+
+app.get('/article-three', function(req, res) {
+    res.send('article-three requested and served');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
