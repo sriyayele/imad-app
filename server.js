b240bbe43251242a6a77a6a7e6e8b704483136c6
sriyayele <30803620+sriyayele@users.noreply.github.com>
2017-08-14 22:26:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,14 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title: 'article one',
+  heading: 'Article one',
+  date: 'aug 14, 2016',
+  content: `<p>
+                this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. 
+            </p>`
+    
+};
+function createTemplate (data) {
+  var title = data.title;
+  var date = data.date;
+  var heading  = data.heading;
+  var content = data.content;
 
-
-
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale-1" />
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home
+        </a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article_one', function(req, res) {
-    res.send('article_one requested and served');
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req, res) {
     res.send('article-two requested and served');
