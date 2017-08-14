ea467e34e13f0669268f8a49dad225c2325aa88e
sriyayele <30803620+sriyayele@users.noreply.github.com>
2017-08-14 20:53:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,71 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-'article_one': {
-    title: 'article one',
-    heading: 'Article one',
-    date: 'aug 11,2017',
-    content: `<p>
-                this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. 
-            </p>`
-},
-'article-two':  {
-    title: 'article two',
-    heading: 'Article two',
-    date: 'aug 13,2017',
-    content: `<p>
-                this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. this is the content in my webapp. 
-            </p>`}
-};
- Function createTemplate (data) 
- {
-    var title =data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-var htmlTemplate = `<html>
-    <head>
-        <title>
-            ${title}
-        
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale-1" />
-       <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home
-        </a>
-        </div>
-        <hr/>
-        <h3>
-           ${heading}
-        </h3>
-        <div>
-            ${date}
-                </div>
-        <div>
-            ${content}
-            </div>
-        </div>
-    </body>
-</html>
-
-`;
-return htmlTemplate;
-}
 
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function(req, res)
-{
-    var articleName = req.params.aticleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article_one', function(req, res) {
+    res.send('article_one requested and served');
+});
+app.get('/article-two', function(req, res) {
+    res.send('article-two requested and served');
 });
 
 
