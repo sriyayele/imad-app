daeef903a109f68974778c999243f6be79a2a0ee
sriyayele <30803620+sriyayele@users.noreply.github.com>
2017-08-19 19:47:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/test-db', function (req,res)
 });
 app.get('/article/:articleName', function(req, res) {
 
-pool.query("SELECT * FROM article_one WHERE title = " + req.params.articleName, function (err, result)
+pool.query("SELECT * FROM article_one WHERE title = '" + req.params.articleName + "'", function (err, result)
 {  if(err){
             res.status(500).send(err.toString());
             
